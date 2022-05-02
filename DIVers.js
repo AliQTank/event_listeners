782e9809f8a66b7add58e5a1bf06e2d4b6415daf
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-02 09:15:06 -0500

    Update DIVers.js

--- DIVers.js
+++ DIVers.js
@@ -10,4 +10,7 @@ const newDiv = document.createElement("div");
 newDiv.style.width = "200px";
 newDiv.style.height = "200px";
 newDiv.style.backgroundColor = "purple"
+newDiv.addEventListener("click", () => {
+    console.log("HI")
+})
 document.body.append(newDiv);
