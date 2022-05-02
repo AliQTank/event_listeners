c88a9c07ff90abcf158172f1ce189475abd24cd1
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-02 08:55:53 -0500

    Update event_listeners.js

--- event_listeners.js
+++ event_listeners.js
@@ -8,14 +8,18 @@ grandparent.addEventListener("click", e => {
     console.log("grandparent bubble");
 })
 
-parent.addEventListener("click", e => {
-    console.log("parent bubble");
-},
-{once: true})
+parent.addEventListener("click", printHi)
+
+setTimeout(() => {
+    parent.removeEventListener("click", printHi)
+}, 2000)
 
 child.addEventListener("click", e => {
     console.log("child bubble");
     
 })
 
+function printHi(){
+    console.log("HI")
+}
 
