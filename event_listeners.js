ca2fbd71b2eae86f0b596a77adda9e2d030af394
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-02 08:20:05 -0500

    Update event_listeners.js

--- event_listeners.js
+++ event_listeners.js
@@ -3,17 +3,28 @@ const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
 
 grandparent.addEventListener("click", e => {
-    console.log("grandparent one");
+    console.log("grandparent capture");
 },
 {capture: true})
 
 parent.addEventListener("click", e => {
-    console.log("parent one");
+    console.log("parent capture");
+},
+{capture: true})
+
+child.addEventListener("click", e => {
+    console.log("child capture");
+},
+{capture: true})
+
+grandparent.addEventListener("click", e => {
+    console.log("grandparent bubble");
+})
+
+parent.addEventListener("click", e => {
+    console.log("parent bubble");
 })
 
 child.addEventListener("click", e => {
-    console.log("child one");
+    console.log("child bubble");
 })
-
-
-
