251838e52e5ae462b017adcb9a38f26477a812dd
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-05-02 12:05:23 -0500

    Update DIVers.js

--- DIVers.js
+++ DIVers.js
@@ -1,12 +1,14 @@
 const divs = document.querySelectorAll("div");
 
-document.addEventListener("click", e => {
-    if (e.target.matches("div")) {
-        console.log("hi");
-    }
-    
-
+addGlobalEventListener("click", "div", e => {
+    console.log("hi")
 })
+
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector)) callback(e)
+    })
+}
 
 const newDiv = document.createElement("div");
 newDiv.style.width = "200px";
